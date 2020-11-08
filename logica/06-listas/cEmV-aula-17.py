adef3da155f380a90806f637537b70db1018e75e
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-11-08 08:07:55 -0300

    Praticando listas

---
+++ logica/06-listas/cEmV-aula-17.py
@@ -0,0 +1,9 @@
+valores = []
+
+valores.append(5)
+valores.append(7)
+valores.append(9)
+
+for c, v in enumerate(valores):
+    print('Na posição %d encontrei o valor %d' % (c, v))
+print('Cheguei no final da lista')
